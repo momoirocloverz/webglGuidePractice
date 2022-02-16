66d9f925abb2d56e9f4f958840e79ca29e1a718a
momoclo <1796583678@qq.com>
2022-02-16 13:28:39 +0800

    fix

--- src/views/tab/mainHome.vue
+++ src/views/tab/mainHome.vue
@@ -47,11 +47,11 @@ export default {
 
       const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
       moonLight.position.set(4, 5, -2)
-
       gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
-
+      gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001)
+      gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001)
+      gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001)
       scene.add(moonLight)
-
       const sizes = {
         width: window.innerWidth,
         height: window.innerHeight
